4b0701f95fca8620e19ccef544d72692f9bced9d
brunoseba <brunor719@gmail.com>
2023-03-09 13:22:13 -0300

    update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,14 +18,18 @@ var (
 	ctx    context.Context
 
 	//User Services, controllers, routes
-	userCollection *mongo.Collection
+	userCollection  *mongo.Collection
+	eventCollection *mongo.Collection
 
 	userService         usecase.UserUseCase
 	UserController      controller.UserController
 	UserRouteController route.UserRouteController
 	UserRepository      repository.UserRepositoryImp
 
-	//EventRepository repository.EventRepository
+	eventService         usecase.EventUseCase
+	EventController      controller.EventController
+	EventRouteController route.EventRouteConstroller
+	EventRepository      repository.EventRepositoryImp
 )
 
 func init() {
@@ -37,12 +41,18 @@ func init() {
 
 	//Collections
 	userCollection = mongoclient.Database("eventmanagement").Collection("users")
+	eventCollection = mongoclient.Database("eventmanagement").Collection("event")
 
 	//Route, Controller and Service --------------------------------------------------
 	UserRepository = repository.NewUserRepository(userCollection, ctx)
 	userService = usecase.NewUserUseCase(UserRepository)
 	UserController = controller.NewUserController(userService)
 	UserRouteController = route.NewUserRouteController(UserController)
+
+	EventRepository = repository.NewEventReposiroty(eventCollection, ctx)
+	eventService = usecase.NewEventUseCase(EventRepository)
+	EventController = controller.NewEventController(eventService)
+	EventRouteController = route.NewEventRouteController(EventController)
 
 	// -------------------------------------------------------------------------------
 
@@ -55,6 +65,7 @@ func main() {
 	// Routes
 	router := server.Group("/api")
 	UserRouteController.UserRoute(router)
+	EventRouteController.EventRoute(router)
 
 	log.Fatal(server.Run(":8090"))
 }
